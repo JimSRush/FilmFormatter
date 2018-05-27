e29c0fdbd887fbb03ab5fd6e309b85d2e5ca9207
JimSRush <jim@galesburg.co.nz>
2018-05-27 14:45:04 +1200

    Added threading

--- FilmFormatter/Form1.cs
+++ FilmFormatter/Form1.cs
@@ -12,6 +12,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Threading;
+
+
 using System.Windows.Forms;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
@@ -21,9 +23,6 @@ namespace FilmFormatter
 {
 	public partial class Form1 : Form
 	{
-
-		List<Tuple<string, int>> titlesToRunTime = new List<Tuple<string, int>>();
-		Dictionary<String, List<String>> citiesToVenues = new Dictionary<String, List<String>>();
 
 		public Form1()
 		{
@@ -43,22 +42,6 @@ namespace FilmFormatter
 		}
 
 
-		private void threadFilmsByTitle(Tuple<string, List<TitleSessionInfo>> t) 
-		{
-			var city = t.Item1;
-			var films = t.Item2;
-			List<Dictionary<String, List<TitleSessionInfo>>> filmsByTitle = parseFilmsByTitleForCity(city, films);
-			writeOutTitlesToFile(filmsByTitle, city);
-
-		}
-
-		private void threadFilmsByDate(Tuple<string, List<TitleSessionInfo>> t)
-		{
-			var city = t.Item1;
-			var films = t.Item2;
-			List<Dictionary<DateTime, List<TitleSessionInfo>>> filmsByDate = parseFilmsByDateByCity(city, films);
-			writeOutDatesToFile(filmsByDate, city);
-		}
 
 
 		private void loadFile(String fileName)
@@ -73,7 +56,7 @@ namespace FilmFormatter
 				using (SpreadsheetDocument myDoc = SpreadsheetDocument.Open(fs, false))
 				{
 					WorkbookPart workbookPart = myDoc.WorkbookPart;
-					titlesToRunTime = GetTitlesFromRuntime(GetWorkSheetFromSheetName(workbookPart, "MAIN"), workbookPart);
+					FilmFormatter.Tools.SpreadSheetWorkers.titlesToRunTime = GetTitlesFromRuntime(GetWorkSheetFromSheetName(workbookPart, "MAIN"), workbookPart);
 					//parse main sheet
 					WorksheetPart worksheetPart = GetWorkSheetFromSheetName(workbookPart, "SCREENING INFO");
 					SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().Last();
@@ -88,16 +71,9 @@ namespace FilmFormatter
 					List<TitleSessionInfo> rawFilmsForOrderByDate = rawFilms.OrderBy(x => x.getDateTimeAsDate()).ThenBy(y => y.getTimeSpan()).ToList();
 					foreach (String city in cities)
 					{
-						var t = System.Tuple.Create(city, rawFilmsForOrderByDate);
-
-						//Thread th = new Thread(threadFilmsByDate);
-
-						//Thread th = new Thread(new ParameterizedThreadStart(threadFilmsByTitle));
-						//th.Start(t);
-
-
-						threadFilmsByTitle(t);
-						threadFilmsByDate(t);
+						var t = System.Tuple.Create(city, rawFilmsForOrderByDate);	
+						FilmFormatter.Tools.SpreadSheetWorkers.threadFilmsByDate(t);
+						FilmFormatter.Tools.SpreadSheetWorkers.threadFilmsByTitle(t);
 					}
 					watch.Stop();
 
@@ -107,179 +83,6 @@ namespace FilmFormatter
 				}
 			}
 		}
-
-
-		private List<Dictionary<DateTime, List<TitleSessionInfo>>> parseFilmsByDateByCity(String city, List<TitleSessionInfo> rawFilms)
-		{
-			List<Dictionary<DateTime, List<TitleSessionInfo>>> filmsByCityByDate = new List<Dictionary<DateTime, List<TitleSessionInfo>>>();
-			foreach (TitleSessionInfo session in rawFilms)
-			{
-				if (session.getCity() == city)
-				{
-					if (!filmsByCityByDate.Any(dic => dic.ContainsKey(session.getDateTimeAsDate())))
-					{
-						Dictionary<DateTime, List<TitleSessionInfo>> toAdd = new Dictionary<DateTime, List<TitleSessionInfo>>() 
-						{
-							{session.getDateTimeAsDate(), new List<TitleSessionInfo>(){session}}
-						};
-						filmsByCityByDate.Add(toAdd);
-					}
-					else
-					{
-						foreach (Dictionary<DateTime, List<TitleSessionInfo>> dict in filmsByCityByDate)
-						{
-							if (dict.ContainsKey(session.getDateTimeAsDate()))
-							{
-								List<TitleSessionInfo> toUpdate = dict[session.getDateTimeAsDate()];
-								toUpdate.Add(session);
-								dict[session.getDateTimeAsDate()] = toUpdate;
-							}
-						}
-					}
-				}
-			}
-
-			return filmsByCityByDate;
-		}
-
-
-
-		private void writeOutDatesToFile(List<Dictionary<DateTime, List<TitleSessionInfo>>> filmsByDate, String city)
-		{
-			String outPutFolder = @"C:\Temp\";
-			System.IO.Directory.CreateDirectory(outPutFolder);
-			using (System.IO.StreamWriter file = new System.IO.StreamWriter(outPutFolder + city + "filmsByDate.txt"))
-			{
-				foreach (Dictionary<DateTime, List<TitleSessionInfo>> date in filmsByDate)
-				{
-					String newDate = setDateAsString(date.Keys.First());
-
-					file.WriteLine(newDate);
-					foreach (List<TitleSessionInfo> value in date.Values)
-					{
-						foreach (TitleSessionInfo cs in value)
-						{
-							//find runtime
-							String shortRunTime = "";
-								//For the bigger cities
-							if (citiesToVenues[city].Count > 1) {
-								if (cs.getShort().Equals("NO SHORT", StringComparison.InvariantCultureIgnoreCase))
-								{
-									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + cs.getVenue() + ") " + getRunTimeFromTitle(cs.getTitle()) + "\t" + cs.getPageNumber();
-									file.WriteLine(toWrite);
-								}
-								else if (!cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("OUTWARDS", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("INWARDS", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("FILMMAKER PRESENT", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase))
-								{
-									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + cs.getVenue() + ") " + getRunTimeFromTitle(cs.getTitle()) + " + " + getRunTimeFromTitle(cs.getShort()) + "\t" + cs.getPageNumber();
-									file.WriteLine(toWrite);
-									
-								}
-								else
-								{
-									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + cs.getVenue() + ") " + getRunTimeFromTitle(cs.getTitle()) + " + " + getRunTimeFromTitle(cs.getShort()) + "\t" + cs.getPageNumber();
-									file.WriteLine(toWrite);
-									
-								}
-								
-							}
-							else { //this is for the single venuie cities
-								if (cs.getShort().Equals("NO SHORT", StringComparison.InvariantCultureIgnoreCase))
-								{
-									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + getRunTimeFromTitle(cs.getTitle()) + ") " + "\t" + cs.getPageNumber();
-									
-								}
-								else if (!cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("OUTWARDS", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("INWARDS", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("FILMMAKER PRESENT", StringComparison.InvariantCultureIgnoreCase)
-								  && !cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase))
-								{
-									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + getRunTimeFromTitle(cs.getTitle()) + " + " + getRunTimeFromTitle(cs.getShort()) + ") " + "\t" + cs.getPageNumber();
-									file.WriteLine(toWrite);
-								}
-								else
-								{
-									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + getRunTimeFromTitle(cs.getTitle()) + ") " + "\t" + cs.getPageNumber();
-									file.WriteLine(toWrite);
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-		private String setDateAsString(DateTime filmDate)
-		{
-			return String.Format("{0:dddd d MMMM}", filmDate);
-		}
-
-
-		private void writeOutTitlesToFile(List<Dictionary<String, List<TitleSessionInfo>>> filmsByTitle, String city)
-		{
-			String outPutFolder = @"C:\Temp\";
-			System.IO.Directory.CreateDirectory(outPutFolder);
-			using (System.IO.StreamWriter file = new System.IO.StreamWriter(outPutFolder + city + "filmsByTitle.txt"))
-			{
-				foreach (Dictionary<String, List<TitleSessionInfo>> film in filmsByTitle)
-				{
-					String title = film.Keys.First();
-					file.WriteLine(title);
-
-					//Iterate the values -- have to get the values by the ey
-					foreach (List<TitleSessionInfo> value in film.Values)
-					{
-						foreach (TitleSessionInfo currentSession in value)
-						{
-							//finally
-							String toWrite = currentSession.getSessionType() + "\t" + currentSession.getVenue() + "\t" + currentSession.getDate() + ", " + currentSession.getTime();
-							file.WriteLine(toWrite);
-						}
-					}
-				}
-			}
-		}
-
-		private List<Dictionary<String, List<TitleSessionInfo>>> parseFilmsByTitleForCity(String city, List<TitleSessionInfo> rawFilms)
-		{
-			List<Dictionary<String, List<TitleSessionInfo>>> filmByCity = new List<Dictionary<String, List<TitleSessionInfo>>>();
-			foreach (TitleSessionInfo session in rawFilms)
-			{
-				if (session.getCity() == city)
-				{
-					if (!filmByCity.Any(dic => dic.ContainsKey(session.getTitle())))
-					{
-						Dictionary<String, List<TitleSessionInfo>> toAdd = new Dictionary<string, List<TitleSessionInfo>>() 
-						{
-							{session.getTitle(), new List<TitleSessionInfo>(){session}}
-						};
-						filmByCity.Add(toAdd);
-					}
-					else
-					{
-						foreach (Dictionary<String, List<TitleSessionInfo>> dict in filmByCity)
-						{
-							if (dict.ContainsKey(session.getTitle()))
-							{
-								List<TitleSessionInfo> toUpdate = dict[session.getTitle()];
-								toUpdate.Add(session);
-								dict[session.getTitle()] = toUpdate;
-
-							}
-						}
-
-					}
-
-				}
-			}
-			return filmByCity;
-		}
-
-
 
 		private List<TitleSessionInfo> parseFilms(SheetData sheetData, WorkbookPart workbookpart)
 		{
@@ -364,30 +167,9 @@ namespace FilmFormatter
 					}
 				}
 			}
-			setCitiesToVenues(rawSchedule);
+			FilmFormatter.Tools.SpreadSheetWorkers.setCitiesToVenues(rawSchedule);
 
 			return rawSchedule;
-		}
-
-		private void setCitiesToVenues (List<TitleSessionInfo> rawSchedule) 
-		{
-		foreach (TitleSessionInfo session in rawSchedule)
-			{
-				if (!citiesToVenues.ContainsKey(session.getCity()))
-				{
-					List<String> values = new List<String>();
-					values.Add(session.getVenue());
-					citiesToVenues.Add(session.getCity(), values);
-				}
-				else
-				{ //the city exists, so we need to check for the value in the values 
-					if (!citiesToVenues[session.getCity()].Contains(session.getVenue()))
-					{
-						citiesToVenues[session.getCity()].Add(session.getVenue());
-					}
-
-				}
-			}
 		}
 
 		private WorksheetPart GetWorkSheetFromSheetName(WorkbookPart workbookpart, String sheetName)
@@ -395,18 +177,6 @@ namespace FilmFormatter
 			Sheet sheet = workbookpart.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name == sheetName);
 			if (sheet == null) throw new Exception(string.Format("Could not find sheet with name {0}", sheetName));
 			else return workbookpart.GetPartById(sheet.Id) as WorksheetPart;
-		}
-
-		private int getRunTimeFromTitle(String title)
-		{
-			foreach (Tuple<String, int> tuple in titlesToRunTime)
-			{
-				if (tuple.Item1 == title)
-				{
-					return tuple.Item2;
-				}
-			}
-			return 0;
 		}
 
 		private List<Tuple<String, int>> GetTitlesFromRuntime(WorksheetPart worksheetpart, WorkbookPart workbookpart)
@@ -441,6 +211,7 @@ namespace FilmFormatter
 					}
 				}
 			}
+
 			return ttrt;
 		}
 
---
+++ FilmFormatter/Tools/SpreadSheetWorkers.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FilmFormatter.Tools
+{
+	class SpreadSheetWorkers
+	{
+
+		public static List<Tuple<string, int>> titlesToRunTime = new List<Tuple<string, int>>();
+		public static  Dictionary<String, List<String>> citiesToVenues = new Dictionary<String, List<String>>();
+
+		public static List<Dictionary<String, List<TitleSessionInfo>>> parseFilmsByTitleForCity(String city, List<TitleSessionInfo> rawFilms)
+		{
+			List<Dictionary<String, List<TitleSessionInfo>>> filmByCity = new List<Dictionary<String, List<TitleSessionInfo>>>();
+			foreach (TitleSessionInfo session in rawFilms)
+			{
+				if (session.getCity() == city)
+				{
+					if (!filmByCity.Any(dic => dic.ContainsKey(session.getTitle())))
+					{
+						Dictionary<String, List<TitleSessionInfo>> toAdd = new Dictionary<string, List<TitleSessionInfo>>() 
+						{
+							{session.getTitle(), new List<TitleSessionInfo>(){session}}
+						};
+						filmByCity.Add(toAdd);
+					}
+					else
+					{
+						foreach (Dictionary<String, List<TitleSessionInfo>> dict in filmByCity)
+						{
+							if (dict.ContainsKey(session.getTitle()))
+							{
+								List<TitleSessionInfo> toUpdate = dict[session.getTitle()];
+								toUpdate.Add(session);
+								dict[session.getTitle()] = toUpdate;
+
+							}
+						}
+
+					}
+
+				}
+			}
+			return filmByCity;
+		}
+
+		public static List<Dictionary<DateTime, List<TitleSessionInfo>>> parseFilmsByDateByCity(String city, List<TitleSessionInfo> rawFilms)
+		{
+			List<Dictionary<DateTime, List<TitleSessionInfo>>> filmsByCityByDate = new List<Dictionary<DateTime, List<TitleSessionInfo>>>();
+			foreach (TitleSessionInfo session in rawFilms)
+			{
+				if (session.getCity() == city)
+				{
+					if (!filmsByCityByDate.Any(dic => dic.ContainsKey(session.getDateTimeAsDate())))
+					{
+						Dictionary<DateTime, List<TitleSessionInfo>> toAdd = new Dictionary<DateTime, List<TitleSessionInfo>>() 
+						{
+							{session.getDateTimeAsDate(), new List<TitleSessionInfo>(){session}}
+						};
+						filmsByCityByDate.Add(toAdd);
+					}
+					else
+					{
+						foreach (Dictionary<DateTime, List<TitleSessionInfo>> dict in filmsByCityByDate)
+						{
+							if (dict.ContainsKey(session.getDateTimeAsDate()))
+							{
+								List<TitleSessionInfo> toUpdate = dict[session.getDateTimeAsDate()];
+								toUpdate.Add(session);
+								dict[session.getDateTimeAsDate()] = toUpdate;
+							}
+						}
+					}
+				}
+			}
+
+			return filmsByCityByDate;
+		}
+
+		private static void DateStart(Tuple<string, List<TitleSessionInfo>> films)
+		{
+			var city = films.Item1;
+			var f = films.Item2;
+			var filmsByDate = parseFilmsByDateByCity(city, f);
+			writeOutDatesToFile(filmsByDate, city);
+		}
+		
+		private static void TitleStart(Tuple<string, List<TitleSessionInfo>>  films)
+		{
+			var city = films.Item1;
+			var f = films.Item2;
+			List<Dictionary<String, List<TitleSessionInfo>>> filmsByTitle = parseFilmsByTitleForCity(city, f);
+			writeOutTitlesToFile(filmsByTitle, city);
+		}
+
+		public static Thread threadFilmsByDate(Tuple<string, List<TitleSessionInfo>> films)
+		{
+			var f = films;
+			var t = new Thread(() => DateStart(f));
+			t.Start();
+			return t;
+		}
+
+		public static Thread threadFilmsByTitle(Tuple<string, List<TitleSessionInfo>> films)
+		{
+			var f = films;
+			var t = new Thread(() => TitleStart(f));
+			t.Start();
+			return t;
+		}
+
+
+		public static String setDateAsString(DateTime filmDate)
+		{
+			return String.Format("{0:dddd d MMMM}", filmDate);
+		}
+
+		public static void setCitiesToVenues(List<TitleSessionInfo> rawSchedule)
+		{
+			foreach (TitleSessionInfo session in rawSchedule)
+			{
+				if (!citiesToVenues.ContainsKey(session.getCity()))
+				{
+					List<String> values = new List<String>();
+					values.Add(session.getVenue());
+					citiesToVenues.Add(session.getCity(), values);
+				}
+				else
+				{ //the city exists, so we need to check for the value in the values 
+					if (!citiesToVenues[session.getCity()].Contains(session.getVenue()))
+					{
+						citiesToVenues[session.getCity()].Add(session.getVenue());
+					}
+
+				}
+			}
+		}
+
+		public static int getRunTimeFromTitle(String title)
+		{
+			foreach (Tuple<String, int> tuple in titlesToRunTime)
+			{
+				if (tuple.Item1 == title)
+				{
+					return tuple.Item2;
+				}
+			}
+			return 0;
+		}
+
+
+		public static void writeOutDatesToFile(List<Dictionary<DateTime, List<TitleSessionInfo>>> filmsByDate, String city)
+		{
+			String outPutFolder = @"C:\Temp\";
+			System.IO.Directory.CreateDirectory(outPutFolder);
+			using (System.IO.StreamWriter file = new System.IO.StreamWriter(outPutFolder + city + "filmsByDate.txt"))
+			{
+				foreach (Dictionary<DateTime, List<TitleSessionInfo>> date in filmsByDate)
+				{
+					String newDate = setDateAsString(date.Keys.First());
+
+					file.WriteLine(newDate);
+					foreach (List<TitleSessionInfo> value in date.Values)
+					{
+						foreach (TitleSessionInfo cs in value)
+						{
+							//find runtime
+							String shortRunTime = "";
+								//For the bigger cities
+							if (citiesToVenues[city].Count > 1) {
+								if (cs.getShort().Equals("NO SHORT", StringComparison.InvariantCultureIgnoreCase))
+								{
+									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + cs.getVenue() + ") " + getRunTimeFromTitle(cs.getTitle()) + "\t" + cs.getPageNumber();
+									file.WriteLine(toWrite);
+								}
+								else if (!cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("OUTWARDS", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("INWARDS", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("FILMMAKER PRESENT", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase))
+								{
+									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + cs.getVenue() + ") " + getRunTimeFromTitle(cs.getTitle()) + " + " + getRunTimeFromTitle(cs.getShort()) + "\t" + cs.getPageNumber();
+									file.WriteLine(toWrite);
+									
+								}
+								else
+								{
+									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + cs.getVenue() + ") " + getRunTimeFromTitle(cs.getTitle()) + " + " + getRunTimeFromTitle(cs.getShort()) + "\t" + cs.getPageNumber();
+									file.WriteLine(toWrite);
+									
+								}
+								
+							}
+							else { //this is for the single venuie cities
+								if (cs.getShort().Equals("NO SHORT", StringComparison.InvariantCultureIgnoreCase))
+								{
+									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + getRunTimeFromTitle(cs.getTitle()) + ") " + "\t" + cs.getPageNumber();
+									
+								}
+								else if (!cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("OUTWARDS", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("INWARDS", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("FILMMAKER PRESENT", StringComparison.InvariantCultureIgnoreCase)
+								  && !cs.getShort().Equals("INTERMISSION", StringComparison.InvariantCultureIgnoreCase))
+								{
+									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + getRunTimeFromTitle(cs.getTitle()) + " + " + getRunTimeFromTitle(cs.getShort()) + ") " + "\t" + cs.getPageNumber();
+									file.WriteLine(toWrite);
+								}
+								else
+								{
+									String toWrite = cs.getSessionType() + "\t" + cs.getTime() + "\t" + cs.getTitle() + " (" + getRunTimeFromTitle(cs.getTitle()) + ") " + "\t" + cs.getPageNumber();
+									file.WriteLine(toWrite);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		public static void writeOutTitlesToFile(List<Dictionary<String, List<TitleSessionInfo>>> filmsByTitle, String city)
+		{
+			String outPutFolder = @"C:\Temp\";
+			System.IO.Directory.CreateDirectory(outPutFolder);
+			using (System.IO.StreamWriter file = new System.IO.StreamWriter(outPutFolder + city + "filmsByTitle.txt"))
+			{
+				foreach (Dictionary<String, List<TitleSessionInfo>> film in filmsByTitle)
+				{
+					String title = film.Keys.First();
+					file.WriteLine(title);
+
+					//Iterate the values -- have to get the values by the ey
+					foreach (List<TitleSessionInfo> value in film.Values)
+					{
+						foreach (TitleSessionInfo currentSession in value)
+						{
+							//finally
+							String toWrite = currentSession.getSessionType() + "\t" + currentSession.getVenue() + "\t" + currentSession.getDate() + ", " + currentSession.getTime();
+							file.WriteLine(toWrite);
+						}
+					}
+				}
+			}
+		}
+
+
+	}
+}
